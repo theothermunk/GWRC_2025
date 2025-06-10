9f1667bb9a78836330c56bedfabd4b576b248469
theothermunk <71202979+theothermunk@users.noreply.github.com>
2025-06-10 13:18:18 +1200

    Initial commit web map files

---
+++ layers/layers.js
@@ -0,0 +1,90 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' &nbsp &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_Districtboundaries_1 = new ol.format.GeoJSON();
+var features_Districtboundaries_1 = format_Districtboundaries_1.readFeatures(json_Districtboundaries_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Districtboundaries_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Districtboundaries_1.addFeatures(features_Districtboundaries_1);
+var lyr_Districtboundaries_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Districtboundaries_1, 
+                style: style_Districtboundaries_1,
+                popuplayertitle: 'District boundaries',
+                interactive: false,
+                title: '<img src="styles/legend/Districtboundaries_1.png" /> District boundaries'
+            });
+var format_shareofgrowth20232033_2 = new ol.format.GeoJSON();
+var features_shareofgrowth20232033_2 = format_shareofgrowth20232033_2.readFeatures(json_shareofgrowth20232033_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_shareofgrowth20232033_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_shareofgrowth20232033_2.addFeatures(features_shareofgrowth20232033_2);
+var lyr_shareofgrowth20232033_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_shareofgrowth20232033_2, 
+                style: style_shareofgrowth20232033_2,
+                popuplayertitle: '% share of growth 2023-2033',
+                interactive: true,
+    title: '% share of growth 2023-2033<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_0.png" /> -5 or less<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_1.png" /> -5 to 0<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_2.png" /> 0 to 2<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_3.png" /> 2 to 5<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_4.png" /> 5 to 10<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_5.png" /> 10 to 15<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_6.png" /> 15 to 20<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_7.png" /> 20 to 25<br />\
+    <img src="styles/legend/shareofgrowth20232033_2_8.png" /> 25 or more<br />' });
+var format_shareofgrowth20232053_3 = new ol.format.GeoJSON();
+var features_shareofgrowth20232053_3 = format_shareofgrowth20232053_3.readFeatures(json_shareofgrowth20232053_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_shareofgrowth20232053_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_shareofgrowth20232053_3.addFeatures(features_shareofgrowth20232053_3);
+var lyr_shareofgrowth20232053_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_shareofgrowth20232053_3, 
+                style: style_shareofgrowth20232053_3,
+                popuplayertitle: '% share of growth 2023-2053',
+                interactive: true,
+    title: '% share of growth 2023-2053<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_0.png" /> -5 or less<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_1.png" /> -5 to 0<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_2.png" /> 0 to 2<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_3.png" /> 2 to 5<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_4.png" /> 5 to 10<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_5.png" /> 10 to 15<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_6.png" /> 15 to 20<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_7.png" /> 20 to 25<br />\
+    <img src="styles/legend/shareofgrowth20232053_3_8.png" /> 25 or more<br />' });
+
+lyr_OSMStandard_0.setVisible(true);lyr_Districtboundaries_1.setVisible(true);lyr_shareofgrowth20232033_2.setVisible(false);lyr_shareofgrowth20232053_3.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_Districtboundaries_1,lyr_shareofgrowth20232033_2,lyr_shareofgrowth20232053_3];
+lyr_Districtboundaries_1.set('fieldAliases', {'TA2023_V1_00': 'TA2023_V1_00', 'TA2023_V1_00_NAME': 'Territorial Authority', 'TA2023_V1_00_NAME_ASCII': 'TA2023_V1_00_NAME_ASCII', 'LAND_AREA_SQ_KM': 'LAND_AREA_SQ_KM', 'AREA_SQ_KM': 'AREA_SQ_KM', 'Shape_Length': 'Shape_Length', 'Shape_Area': 'Shape_Area', });
+lyr_shareofgrowth20232033_2.set('fieldAliases', {'SA22023_V1_00': 'SA22023_V1_00', 'SA22023_V1_00_NAME': 'SA2', 'SA22023_V1_00_NAME_ASCII': 'SA22023_V1_00_NAME_ASCII', 'AREA_SQ_KM': 'AREA_SQ_KM', 'LAND_AREA_SQ_KM': 'LAND_AREA_SQ_KM', 'Shape_Length': 'Shape_Length', 'Shape_Area': 'Shape_Area', 'TALB2023_V1_00': 'TALB2023_V1_00', 'TALB2023_V1_00_NAME': 'TALB2023_V1_00_NAME', 'TALB2023_V1_00_NAME_ASCII': 'TA', 'LAND_AREA_SQ_KM_2': 'LAND_AREA_SQ_KM_2', 'AREA_SQ_KM_2': 'AREA_SQ_KM_2', 'Shape_Length_2': 'Shape_Length_2', 'Shape_Area_2': 'Shape_Area_2', 'GrowthGroup': 'GrowthGroup', 'ShortTerm_Area': 'ShortTerm_Area', 'ShortTerm_SA2_name': 'ShortTerm_SA2_name', 'ShortTerm_Population in 2023': '2023 Population', 'ShortTerm_Population in 2033': '2033 population', 'ShortTerm_Population change': 'Population change', 'ShortTerm_Population change range': 'ShortTerm_Population change range', 'ShortTerm_Population change % of TA': 'Share of TA growth (%)', 'ShortTerm_Households in 2023': '2023 households', 'ShortTerm_Households in 2033': '2033 households', 'ShortTerm_Household change': 'Change in households', 'ShortTerm_Household change range': 'ShortTerm_Household change range', 'ShortTerm_Household change % of TA': 'ShortTerm_Household change % of TA', 'ShortTerm_Dwellings in 2023': '2023 dwellings', 'ShortTerm_Dwellings in 2033': '2033 dwellings', 'ShortTerm_Dwelling change': 'Change in dwellings', 'ShortTerm_Dwellings change range': 'ShortTerm_Dwellings change range', 'ShortTerm_Dwelling change % of TA': 'ShortTerm_Dwelling change % of TA', });
+lyr_shareofgrowth20232053_3.set('fieldAliases', {'SA22023_V1_00': 'SA22023_V1_00', 'SA22023_V1_00_NAME': 'SA2', 'SA22023_V1_00_NAME_ASCII': 'SA22023_V1_00_NAME_ASCII', 'AREA_SQ_KM': 'AREA_SQ_KM', 'LAND_AREA_SQ_KM': 'LAND_AREA_SQ_KM', 'Shape_Length': 'Shape_Length', 'Shape_Area': 'Shape_Area', 'TALB2023_V1_00': 'TALB2023_V1_00', 'TALB2023_V1_00_NAME': 'TA', 'TALB2023_V1_00_NAME_ASCII': 'TALB2023_V1_00_NAME_ASCII', 'LAND_AREA_SQ_KM_2': 'LAND_AREA_SQ_KM_2', 'AREA_SQ_KM_2': 'AREA_SQ_KM_2', 'Shape_Length_2': 'Shape_Length_2', 'Shape_Area_2': 'Shape_Area_2', 'GrowthGroup': 'GrowthGroup', 'LongTerm_Area': 'LongTerm_Area', 'LongTerm_SA2_name': 'LongTerm_SA2_name', 'LongTerm_Population in 2023': '2023 population', 'LongTerm_Population in 2053': '2053 population', 'LongTerm_Population change': 'Population change', 'LongTerm_Population change range': 'LongTerm_Population change range', 'LongTerm_Population change % of TA': 'Share of TA growth (%)', 'LongTerm_Households in 2023': '2023 households', 'LongTerm_Households in 2053': '2053 households', 'LongTerm_Household change': 'Households change', 'LongTerm_Household change range': 'LongTerm_Household change range', 'LongTerm_Household change % of TA': 'LongTerm_Household change % of TA', 'LongTerm_Dwellings in 2023': '2023 dwellings', 'LongTerm_Dwellings in 2053': '2053 dwellings', 'LongTerm_Dwelling change': 'Dwellings change', 'LongTerm_Dwellings change range': 'LongTerm_Dwellings change range', 'LongTerm_Dwelling change % of TA': 'LongTerm_Dwelling change % of TA', });
+lyr_Districtboundaries_1.set('fieldImages', {'TA2023_V1_00': 'Hidden', 'TA2023_V1_00_NAME': 'TextEdit', 'TA2023_V1_00_NAME_ASCII': 'Hidden', 'LAND_AREA_SQ_KM': 'Hidden', 'AREA_SQ_KM': 'Hidden', 'Shape_Length': 'Hidden', 'Shape_Area': 'Hidden', });
+lyr_shareofgrowth20232033_2.set('fieldImages', {'SA22023_V1_00': 'Hidden', 'SA22023_V1_00_NAME': 'TextEdit', 'SA22023_V1_00_NAME_ASCII': 'Hidden', 'AREA_SQ_KM': 'Hidden', 'LAND_AREA_SQ_KM': 'Hidden', 'Shape_Length': 'Hidden', 'Shape_Area': 'Hidden', 'TALB2023_V1_00': 'Hidden', 'TALB2023_V1_00_NAME': 'Hidden', 'TALB2023_V1_00_NAME_ASCII': 'TextEdit', 'LAND_AREA_SQ_KM_2': 'Hidden', 'AREA_SQ_KM_2': 'Hidden', 'Shape_Length_2': 'Hidden', 'Shape_Area_2': 'Hidden', 'GrowthGroup': 'Hidden', 'ShortTerm_Area': 'Hidden', 'ShortTerm_SA2_name': 'Hidden', 'ShortTerm_Population in 2023': 'TextEdit', 'ShortTerm_Population in 2033': 'TextEdit', 'ShortTerm_Population change': 'TextEdit', 'ShortTerm_Population change range': 'Hidden', 'ShortTerm_Population change % of TA': 'TextEdit', 'ShortTerm_Households in 2023': 'TextEdit', 'ShortTerm_Households in 2033': 'TextEdit', 'ShortTerm_Household change': 'TextEdit', 'ShortTerm_Household change range': 'Hidden', 'ShortTerm_Household change % of TA': 'Hidden', 'ShortTerm_Dwellings in 2023': 'TextEdit', 'ShortTerm_Dwellings in 2033': 'TextEdit', 'ShortTerm_Dwelling change': 'TextEdit', 'ShortTerm_Dwellings change range': 'Hidden', 'ShortTerm_Dwelling change % of TA': 'Hidden', });
+lyr_shareofgrowth20232053_3.set('fieldImages', {'SA22023_V1_00': 'Hidden', 'SA22023_V1_00_NAME': 'TextEdit', 'SA22023_V1_00_NAME_ASCII': 'Hidden', 'AREA_SQ_KM': 'Hidden', 'LAND_AREA_SQ_KM': 'Hidden', 'Shape_Length': 'Hidden', 'Shape_Area': 'Hidden', 'TALB2023_V1_00': 'Hidden', 'TALB2023_V1_00_NAME': 'TextEdit', 'TALB2023_V1_00_NAME_ASCII': 'Hidden', 'LAND_AREA_SQ_KM_2': 'Hidden', 'AREA_SQ_KM_2': 'Hidden', 'Shape_Length_2': 'Hidden', 'Shape_Area_2': 'Hidden', 'GrowthGroup': 'Hidden', 'LongTerm_Area': 'Hidden', 'LongTerm_SA2_name': 'Hidden', 'LongTerm_Population in 2023': 'TextEdit', 'LongTerm_Population in 2053': 'TextEdit', 'LongTerm_Population change': 'TextEdit', 'LongTerm_Population change range': 'Hidden', 'LongTerm_Population change % of TA': 'TextEdit', 'LongTerm_Households in 2023': 'TextEdit', 'LongTerm_Households in 2053': 'TextEdit', 'LongTerm_Household change': 'TextEdit', 'LongTerm_Household change range': 'Hidden', 'LongTerm_Household change % of TA': 'Hidden', 'LongTerm_Dwellings in 2023': 'TextEdit', 'LongTerm_Dwellings in 2053': 'TextEdit', 'LongTerm_Dwelling change': 'TextEdit', 'LongTerm_Dwellings change range': 'Hidden', 'LongTerm_Dwelling change % of TA': 'Hidden', });
+lyr_Districtboundaries_1.set('fieldLabels', {'TA2023_V1_00_NAME': 'no label', });
+lyr_shareofgrowth20232033_2.set('fieldLabels', {'SA22023_V1_00_NAME': 'inline label - visible with data', 'TALB2023_V1_00_NAME_ASCII': 'inline label - visible with data', 'ShortTerm_Population in 2023': 'inline label - visible with data', 'ShortTerm_Population in 2033': 'inline label - visible with data', 'ShortTerm_Population change': 'inline label - visible with data', 'ShortTerm_Population change % of TA': 'inline label - visible with data', 'ShortTerm_Households in 2023': 'inline label - visible with data', 'ShortTerm_Households in 2033': 'inline label - visible with data', 'ShortTerm_Household change': 'inline label - visible with data', 'ShortTerm_Dwellings in 2023': 'inline label - visible with data', 'ShortTerm_Dwellings in 2033': 'inline label - visible with data', 'ShortTerm_Dwelling change': 'inline label - visible with data', });
+lyr_shareofgrowth20232053_3.set('fieldLabels', {'SA22023_V1_00_NAME': 'inline label - visible with data', 'TALB2023_V1_00_NAME': 'inline label - visible with data', 'LongTerm_Population in 2023': 'inline label - visible with data', 'LongTerm_Population in 2053': 'inline label - visible with data', 'LongTerm_Population change': 'inline label - visible with data', 'LongTerm_Population change % of TA': 'inline label - visible with data', 'LongTerm_Households in 2023': 'inline label - visible with data', 'LongTerm_Households in 2053': 'inline label - visible with data', 'LongTerm_Household change': 'inline label - visible with data', 'LongTerm_Dwellings in 2023': 'inline label - visible with data', 'LongTerm_Dwellings in 2053': 'inline label - visible with data', 'LongTerm_Dwelling change': 'inline label - visible with data', });
+lyr_shareofgrowth20232053_3.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/shareofgrowth20232053_3_style.js
@@ -0,0 +1,105 @@
+var size = 0;
+var placement = 'point';
+function categories_shareofgrowth20232053_3(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '-5 or less':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(241,238,246,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '-5 to 0':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(215,220,236,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '0 to 2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(189,201,225,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2 to 5':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(153,185,216,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '5 to 10':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(116,169,207,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '10 to 15':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(80,155,199,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '15 to 20':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(43,140,190,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '20 to 25':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(24,115,166,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '25 or more':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.75)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(4,90,141,0.75)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_shareofgrowth20232053_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("q2wHide_GrowthGroup");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("SA22023_V1_00_NAME") !== null) {
+        labelText = String(feature.get("SA22023_V1_00_NAME"));
+    }
+    
+    var style = categories_shareofgrowth20232053_3(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
